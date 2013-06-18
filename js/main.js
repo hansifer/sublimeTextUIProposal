c25650a65633e68d6670ad9d87afb6790217b680
Hans Meyer <git@hansmeyer.com>
2013-06-18 20:17:21 +0100

    add color theming

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-!function() {
+! function() {
 	var menuShown = false;
 	var filepathShown = false;
 
@@ -6,10 +6,10 @@
 		if (menuShown) {
 			if (!filepathShown) {
 				$('#menubar').addClass('barHidden');
-				$('#code').css('top', '7px');
-				$('#code').css('height', '719px');
-				$('#minimap').css('height', '719px');
-				$('#minimap').css('top', '7px');
+				$('#code').css('top', '0px');
+				$('#code').css('height', '716px');
+				$('#minimap').css('top', '9px');
+				$('#minimap').css('height', '716px');
 				$('#scrollThumb').css('top', '39px');
 			}
 			menuShown = !menuShown;
@@ -22,10 +22,10 @@
 	function showMenu(e) {
 		if (!menuShown) {
 			$('#menubar').removeClass('barHidden');
-			$('#code').css('top', '28px');
-			$('#minimap').css('top', '35px');
-			$('#minimap').css('height', '698px');
+			$('#code').css('top', '19px');
 			$('#code').css('height', '698px');
+			$('#minimap').css('top', '35px');
+			$('#minimap').css('height', '691px');
 			$('#scrollThumb').css('top', '67px');
 			$('#menubarText').css('background-image', 'url("img/menubarText.png")')
 			menuShown = !menuShown;
@@ -40,10 +40,10 @@
 		if (filepathShown) {
 			if (!menuShown) {
 				$('#menubar').addClass('barHidden');
-				$('#code').css('top', '7px');
-				$('#code').css('height', '719px');
-				$('#minimap').css('height', '719px');
-				$('#minimap').css('top', '7px');
+				$('#code').css('top', '0px');
+				$('#code').css('height', '716px');
+				$('#minimap').css('height', '716px');
+				$('#minimap').css('top', '9px');
 				$('#scrollThumb').css('top', '39px');
 			}
 			filepathShown = !filepathShown;
@@ -56,9 +56,9 @@
 	function showFilepath(e) {
 		if (!filepathShown) {
 			$('#menubar').removeClass('barHidden');
-			$('#code').css('top', '28px');
+			$('#code').css('top', '19px');
 			$('#code').css('height', '698px');
-			$('#minimap').css('height', '698px');
+			$('#minimap').css('height', '691px');
 			$('#minimap').css('top', '35px');
 			$('#scrollThumb').css('top', '67px');
 			$('#menubarText').css('background-image', 'url("img/filepathText.png")')
@@ -108,6 +108,95 @@
 		} else {
 			$('#minimap').addClass('minimap');
 		}
+	});
+
+	var themeColorPerm = 4;
+	var themeColors = [
+		['#4e3737', 'hsl(0, 60%, 25%)'],
+		['#4e4e38', 'hsl(58,55%,26%)'],
+		['#36513b', 'hsl(132,69%,26%)'],
+		['#324967', 'hsl(214,90%,38%)'],
+		['#393939', 'hsl(0, 0%, 13%)'],
+		/*['#483842', 'hsl(322,48%,22%)'],*/
+	];
+
+	$('#swatch1').on('click mouseover mouseout', function(e) {
+		if (e.type == 'mouseout') {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[themeColorPerm][1]);
+			$('.contentArea').css('background-color', themeColors[themeColorPerm][0]);
+		} else {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[0][1]);
+			$('.contentArea').css('background-color', themeColors[0][0]);
+
+			if (e.type == 'click') {
+				themeColorPerm = 0;
+			}
+		}
+	});
+
+	$('#swatch2').on('click mouseover mouseout', function(e) {
+		if (e.type == 'mouseout') {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[themeColorPerm][1]);
+			$('.contentArea').css('background-color', themeColors[themeColorPerm][0]);
+		} else {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[1][1]);
+			$('.contentArea').css('background-color', themeColors[1][0]);
+
+			if (e.type == 'click') {
+				themeColorPerm = 1;
+			}
+		}
+	});
+
+	$('#swatch3').on('click mouseover mouseout', function(e) {
+		if (e.type == 'mouseout') {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[themeColorPerm][1]);
+			$('.contentArea').css('background-color', themeColors[themeColorPerm][0]);
+		} else {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[2][1]);
+			$('.contentArea').css('background-color', themeColors[2][0]);
+
+			if (e.type == 'click') {
+				themeColorPerm = 2;
+			}
+		}
+	});
+
+	$('#swatch4').on('click mouseover mouseout', function(e) {
+		if (e.type == 'mouseout') {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[themeColorPerm][1]);
+			$('.contentArea').css('background-color', themeColors[themeColorPerm][0]);
+		} else {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[3][1]);
+			$('.contentArea').css('background-color', themeColors[3][0]);
+
+			if (e.type == 'click') {
+				themeColorPerm = 3;
+			}
+		}
+	});
+
+	$('#swatch5').on('click mouseover mouseout', function(e) {
+		if (e.type == 'mouseout') {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[themeColorPerm][1]);
+			$('.contentArea').css('background-color', themeColors[themeColorPerm][0]);
+		} else {
+			$('.sublimeWindowOverlay').css('background-color', themeColors[4][1]);
+			$('.contentArea').css('background-color', themeColors[4][0]);
+
+			if (e.type == 'click') {
+				themeColorPerm = 4;
+			}
+		}
+	});
+
+	$().ready(function() {
+		$('#swatch1').css('background-color', themeColors[0][0]);
+		$('#swatch2').css('background-color', themeColors[1][0]);
+		$('#swatch3').css('background-color', themeColors[2][0]);
+		$('#swatch4').css('background-color', themeColors[3][0]);
+		$('#swatch5').css('background-color', themeColors[4][0]);
+		$('#contentArea').addClass('contentArea');
 	});
 
 }();
